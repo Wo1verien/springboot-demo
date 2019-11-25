70d74d9d82d21c536a2f10c3e5837c95158ebf6d
Wolverien <wolverien@163.com>
2019-11-25 22:45:06 +0800

    getting started

--- src/main/java/com/logan/demo/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/logan/demo/springbootdemo/SpringbootDemoApplication.java
@@ -2,9 +2,20 @@ package com.logan.demo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Logan
+ */
+@RestController
 @SpringBootApplication
 public class SpringbootDemoApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
