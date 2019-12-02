668ada375d11d11395f97ce3e2a1590d24efffba
changzheng <changzheng@mafengwo.com>
2019-12-02 20:56:19 +0800

    - Using Spring Boot2.3.4.5.6

--- src/main/java/com/logan/demo/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/logan/demo/springbootdemo/SpringbootDemoApplication.java
@@ -2,8 +2,12 @@ package com.logan.demo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+/**
+ * @author changzheng
+ */
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
